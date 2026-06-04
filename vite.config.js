e15a905681a01c05b50d96d2f34284331f706679
Christopher Baker <ShakaCB@Gmail.com>
2026-06-04 00:01:19 -0500

    Add Tailwind CSS support and update dependencies in package.json and vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  base: '/tuxshow-website/' // This must match your repository name EXACTLY
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+  base: '/tuxshow-website/', 
 })
